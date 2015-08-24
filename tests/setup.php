3afd7f5ed927e6a62902c7093268fcd4b67d875c
Luca Tumedei <luca@theaveragedev.com>
2015-08-24 17:54:26 +0200

    initial commmit

---
+++ tests/setup.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once dirname(dirname(__FILE__)) . '/klein.php';
+
+class HeadersEcho extends _Headers {
+	public function header($key, $value = null) {
+		echo $this->_header($key, $value) . "\n";
+	}
+}
+
+_Request::$_headers = _Response::$_headers = new HeadersEcho;
